b6a16f94bf4de977e58beb601c6b661a1bdc99aa
Adham Elwassimy <163461819+adhamelwassimy2001@users.noreply.github.com>
2026-09-21 14:02:19 +0100

    Add new annotator email and spreadsheet link

--- annotation_utils.py
+++ annotation_utils.py
@@ -11,7 +11,8 @@ ANNOTATION_COL  = 3
 ANNOTATION_COL2 = 4
 
 ANNOTATOR_SHEETS = {
-    "adhamcr80@gmail.com":   "https://docs.google.com/spreadsheets/d/1yCjM2mD4tdPhAvdMHYEj6J05OsZQ2B07qRwo2yfavUk/edit?gid=1271897301#gid=1271897301"
+    "adhamcr80@gmail.com":   "https://docs.google.com/spreadsheets/d/1yCjM2mD4tdPhAvdMHYEj6J05OsZQ2B07qRwo2yfavUk/edit?gid=1271897301#gid=1271897301",
+    "kerstyn@tellmi.help":   "https://docs.google.com/spreadsheets/d/1iJOVG-a1BcRK27VBgWLt2J5BPkHaflNwtwyNirYJ81c/edit?gid=1378083309#gid=1378083309"
 }
 
 def find_resume_index(rows, annotation_key):
